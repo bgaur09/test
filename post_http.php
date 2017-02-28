c495641b61629baaa129489ad5a02e52e391b94f
unknown <Basant.Gaur@Basant-Gaur.indiaprepaid.net>
2017-02-28 17:02:32 +0530

    test

--- post_http.php
+++ post_http.php
@@ -1,31 +1,5 @@
 <?php
-
-// db credentials
-$servername = "127.0.0.1";
-$username = "root";
-$password = "password";
-$dbname = "test";
-
-$entityBody = file_get_contents('php://input');
-
-if (isset($entityBody)) {
-    $result = json_decode($entityBody);
-   
-   
-
-$db = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
-$sql = "INSERT INTO save_post_data (oxigen_id, order_id, operator_id, status)
-VALUES ('".$result->OPTRANSID."', '".$result->ORDERID."', '".$result->PRID."', '".$result->STATUS."')";
-$sqlfnl = $db->prepare($sql);
-
-if($sqlfnl->execute() == TRUE) {
-	echo '{"STATUS":"data saved !"}';
-}
-else {
-	echo '{"STATUS":"data could not be saved !"}';
-}
-
-}
+echo("UPDATED");
 
 
 ?>
